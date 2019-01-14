1fbac950b2fee2dc5a78ef421d391c9819497735
President Macaron <macron@elysee.fr>
2019-01-14 21:00:16 +0200

    les croissants sont prêts

--- ocaml/btree-baby.ml
+++ ocaml/btree-baby.ml
@@ -2,12 +2,28 @@ type btree = Nil | Node of (float * float) * btree * btree
 
 let tree1 = Node((4.0, 5.0), Nil, Nil)
 
-let isOrderedTree = Node((3.0, 10.0),
-                         Node((5.0, 8.0), Node((6.0, 7.0), Nil, Nil), Node((4.0, 9.0), Nil, Nil)),
-                         Node((2.0, 12.0), Nil, Node((1.0, 5.0), Nil, Nil)))
+let isOrderedTree =
+    Node((3.0, 10.0),
+         Node((5.0, 8.0), Node((6.0, 7.0), Nil, Nil), Node((4.0, 9.0), Nil, Nil)),
+         Node((2.0, 12.0), Nil, Node((1.0, 15.0), Nil, Nil)))
 
-let orderedTree tree = match tree with
+let inInterval a b node = match node with
+  | Nil -> true
+  | Node((x, y), _, _) -> a >= x && a <= y && b >= a && b <= y
+
+let nodeContains b a = match a with
+  | Nil -> true
+  | Node((ax, ay), _, _) -> inInterval ax ay b
+
+let rec orderedTree tree = match tree with
   | Nil -> true
   | Node(_, Nil, Nil) -> true
-  | Node((iStart, iEnd), nil, right) ->
-  | Node((iStart, iEnd), left, right) ->
+  | Node(_, Nil, right) -> nodeContains right tree && orderedTree right
+  | Node(_, left, Nil) -> nodeContains tree left && orderedTree left
+  | Node(_, left, right) ->
+          nodeContains right tree && nodeContains tree left &&
+          orderedTree right && orderedTree left
+
+let () = match (orderedTree isOrderedTree) with
+  | true -> print_endline "true"
+  | false -> print_endline "false"
